e5390fcf0f7ff12456b061cd6aa351ed0079b4e8
iath-dev <developer.iath@gmail.com>
2020-02-12 09:34:02 -0500

    170. Creando el Formulario

--- src/components/form/index.jsx
+++ src/components/form/index.jsx
@@ -2,7 +2,40 @@ import React from 'react';
 
 const Form = () => {
     return ( 
-        <h1>Formulario</h1>
+        <form className="col-md-12 mt-2">
+            <fieldset className="text-center">
+                <legend>Buscar bebidas por Categoría o Ingrediente</legend>
+            </fieldset>
+
+            <div className="row">
+                <div className="col-md-4">
+                    <input
+                        name="name"
+                        className="form-control"
+                        type="text"
+                        placeholder="Buscar por Ingrediente"
+                    />
+                </div>
+                <div className="col-md-4">
+                    <select
+                        className="form-control"
+                        name="category"
+                    >
+                        <option value="">--Seleccione una Categoría...</option>
+                    </select>
+                </div>
+
+                <div className="col-md-4">
+                    <input
+                        type="submit"
+                        className="btn btn-primary btn-block"
+                        value="Buscar Bebidas"
+
+                    />
+                </div>
+            </div>
+
+        </form>
      );
 }
  
